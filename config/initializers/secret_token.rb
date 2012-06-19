38d4229a7e8d1c7b13ed7cc88ff44e5a0bb3e5c3
Ron Ijack <ijack.ron@gmail.com>
2012-06-19 14:12:16 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoolDev::Application.config.secret_token = '707f445bc7f2e6e31747dc3d01028dd76f75a636f98ccc31b087cba5beb5db19cb9f210eb8b907170108afc5281fa99991d962f381a85e53e800d38af5f71868'
